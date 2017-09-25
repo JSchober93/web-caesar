0a0084b280c7e9f84673d744f3944bfcdac30d19
Mr Sticky <j.schober93@gmail.com>
2017-09-25 10:40:22 -0500

    Modified submission of form to return the encrypted string back into the input box of the textarea

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -32,21 +32,21 @@ form = """
         Rotate by:
         <input type="text" name="rot" value="0"/>
         </label>
-        <textarea name="text">Enter text here...</textarea>
+        <textarea name="text">{0}</textarea>
         <input type="submit" value="Submit Query"/>
     </body>
 </html>
 """
 @app.route("/")
 def index():
-    return form
+    return form.format('')
 
 @app.route("/", methods=['POST'])
 def encrypt():
     text = request.form['text']
     rot = int(request.form['rot'])
     encrypted = rotate_string(text, rot)
-    content = "<h1>" + encrypted + "</h1>"
-    return content
+    content = encrypted
+    return form.format(content)
     
 app.run()
